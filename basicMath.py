88bc3555dc61f439710eff951e8abe4a9c2df33a
nbernstein-teacher <nbernstein@gocfs.net>
2026-02-02 15:15:01 -0500

    Update files with more comments

--- basicMath.py
+++ basicMath.py
@@ -1,22 +1,28 @@
 #Get user inputs
 #TODO: There are 3 errors in here, can you find them?
-print("What numbers would you like to do math with?")
-print("Enter number 1: ")
-num1 = input()
-print("Enter number 2: )
-num1 = int(input())
+print("What numbers would you like to do math with?") #Print an opening message to the user
+print("Enter number 1: ") #Ask the user to input a number
+num1 = input() #Save the input to a variable
+print("Enter number 2: ) #Ask the user to input another number
+num1 = int(input()) #Save the input as another variable
 
 #Multiply and print
-mult = num1 * num2
-print("Multiplied: ", mult)
+mult = num1 * num2 #Save the product of the two variables into mult
+print("Multiplied: ", mult) #Print the result
 
 #Divide and print
 #TODO: There is an error in this segment
-divi == num1/num2
-print("Divided: ", divi)
+divi == num1/num2 #Save the result of dividing the two variables into divi
+print("Divided: ", divi) #Print the result
 
 #TODO: Add addition calculation and print
+    #Save the result of adding the two variables into a variable
+    #Print the result
 
 #TODO: Add subtraction calculation and print
+    #Save the result of subtracting the two variables into a variable
+    #Print the result
 
 #TODO: Now use the "**" operator to add an exponent (power) calculation and print
+    #Save the result of putting one variable to the power of the other (x^z) into a variable
+    #Print the result
--- variables.py
+++ variables.py
@@ -1,14 +1,24 @@
 #Defining our variables
-name = "Mr. Bernstein"
-age = 23
-height = 72.75
-favorite_food = "Pasta"
+name = "Mr. Bernstein" #This is a string because it has quotes around it
+age = 23 #This is an integer (a number) because it doesn't have quotes
+height = 72.75 #This is a float (decimal number) because it doesn't have quotes and has a decimal point.
+favorite_food = "Pasta" #This is another string
+
+age = float(age) #Changing age to a float
+age = int(age) #Changing age to an integer
+age = str(age) #Changing age to a string
 
 #Basic print statement
-print(name)
+print(age)
 
 #How to print the type of a variable
-print(type(name))
+print(type(age))
 
 #How to print a string, with a varible attached to it
-print("Hello, my name is:", name)
+print("Hello, my name is:", name +".") #Comma adds a space between, + doesn't add a space
+
+#Careful! If you add a string and a number, you'll get an error!
+#Example:
+#print("My name is " + name + " I am " + height + " inches tall.") - This results in an error.
+#print("My name is " + name + " I am ", height, " inches tall.") - This works.
+#print("My name is " + name + " I am " + str(height) + " inches tall.") - This also works, because height is changed to a string.
