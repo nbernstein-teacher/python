cdf4ab133922e3996a4bd2744f5eda2cc0f30cfe
nbernstein-teacher <nbernstein@gocfs.net>
2026-04-06 11:20:48 -0400

    Added pygame update

--- pygameintro/pygame1.py
+++ pygameintro/pygame1.py
@@ -4,20 +4,59 @@ pygame.init()
 
 screen = pygame.display.set_mode((640,640))
 
-mario = pygame.image.load('mario.png').convert()
+mario = pygame.image.load('mariomove.gif').convert_alpha()
+#New code to change the size of the image
+mario = pygame.transform.scale(mario, (mario.get_width()*.2, mario.get_height()*.2))
+
+#Add a background image
+backgroundimg = pygame.image.load('background.png')
+#Scale background image
+backgroundimg = pygame.transform.scale(backgroundimg, (backgroundimg.get_width()*3, backgroundimg.get_height()*3))
 
 clock = pygame.time.Clock()
 
 running = True
 x = 0
+#add a y
+y = 0
 while running:
-    screen.blit(mario, (x, 30))
+    #NEW CODE - changes background to white
+    #Also makes it so we refresh the screen every frame
+    #screen.fill((255,255,255))
 
-    x += 1
+    #Add a background image
+    screen.blit(backgroundimg, (0,0))
+
+
+
+    screen.blit(mario, (x, y))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        #NEW CODE FOR KEYMAPS - Individual single presses
+        '''
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+                y -= 5
+            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                y += 5
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                x -= 5
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                x += 5
+        '''
+    #NEW CODE FOR LONG KEY HOLDS
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] or keys[pygame.K_w]:
+        y -= 3
+    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        y += 3
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        x -= 3
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        x += 3
 
     clock.tick(60)
 
--- pygameintro/pygametutorial.py
+++ pygameintro/pygametutorial.py
@@ -2,22 +2,52 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((500,500))
+screen = pygame.display.set_mode((600,336))
 
-baseball = pygame.image.load('baseball.jpg')
+baseball = pygame.image.load('mariomove.gif')
+baseball = pygame.transform.scale(baseball, (baseball.get_width()*.2, baseball.get_height()*.2))
+
+backg = pygame.image.load('background.png')
+backg = pygame.transform.scale(backg, (backg.get_width()*2, backg.get_height()*2))
 
 clock = pygame.time.Clock()
 
 running = True
 x = 0
+y = 240
 while running:
-
-    screen.blit(baseball, (x, 0))
-    x += .5
+    screen.fill((0,0,0))
+    screen.blit(backg, (0,0))
+    screen.blit(baseball, (x, y))
+    #x += .5
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                y = y-5
+        '''
+            if event.key == pygame.K_UP:
+                y = y-1
+            if event.key == pygame.K_DOWN:
+                y += 5
+            if event.key == pygame.K_LEFT:
+                x -= 5
+            if event.key == pygame.K_RIGHT:
+                x += 5
+        '''
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        y = y-1
+    if keys[pygame.K_DOWN]:
+        y = y+1
+    if keys[pygame.K_LEFT]:
+        x = x-1
+    if keys[pygame.K_RIGHT]:
+        x = x+1
+
     clock.tick(60)
 
     pygame.display.flip()
